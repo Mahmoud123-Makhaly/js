1e229ded2bdc4c967469aa31b8c480f615c7a974
mahmoud <mahmoudmakhaly123@gmail.com>
2023-02-06 13:14:59 +0200

    newTemp

---
+++ main.js
@@ -0,0 +1,24 @@
+// class car{
+//     name="bmw";
+//     printColor=(name)=>{
+//         console.log(name)
+//     }
+// };
+// let car1 = new car();
+// console.log(car1.printColor("skoda"))
+
+
+
+class red {
+    printRed = ()=>{
+        console.log("Helaaaaddslo freom red")
+    }
+}
+class car extends red{
+    name="bmw";
+    printCar=()=>{
+        console.log(this.name)
+    }
+}
+let car1 = new car();
+console.log(car1.printRed())
